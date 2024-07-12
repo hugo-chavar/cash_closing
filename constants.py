4b8e4ca8ca48fe312a2b2fb966aef4c3849ff632
Hugo Chavar <hechavar@gmail.com>
2024-07-12 17:57:31 -0300

    Improve constants

--- constants.py
+++ constants.py
@@ -3,10 +3,12 @@ from date_tests import get_format_shortdate
 
 BASE_DATE_TIME = datetime.datetime(2024, 6, 20, 0, 0)
 
-LAST_CASH_POINT_CLOSING_EXPORT_ID = 10
-LAST_RECEIPT_NUMBER = 1033
+LAST_CASH_POINT_CLOSING_EXPORT_ID = 16
+LAST_RECEIPT_NUMBER = 1616
 
-TRANSACTIONS_FILENAME = f'merged_json\\merged_file_filter_{get_format_shortdate(BASE_DATE_TIME + datetime.timedelta(days=LAST_CASH_POINT_CLOSING_EXPORT_ID + 1))}.json'
-CASH_CLOSING_FILENAME = f'closings\\CASH_CLOSING_{LAST_CASH_POINT_CLOSING_EXPORT_ID + 1:03}.json'
+date_fmt = get_format_shortdate(BASE_DATE_TIME + datetime.timedelta(days=LAST_CASH_POINT_CLOSING_EXPORT_ID + 1))
+cc_number = LAST_CASH_POINT_CLOSING_EXPORT_ID + 1
+TRANSACTIONS_FILENAME = f'merged_json\\merged_file_filter_{date_fmt}.json'
+CASH_CLOSING_FILENAME = f'closings\\CASH_CLOSING_{cc_number:03}_{date_fmt}.json'
 CASH_CLOSING_UNFORMATTED_FILENAME = 'resultUNF_prod_0.json'
 
