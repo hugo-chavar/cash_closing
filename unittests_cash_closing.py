81c3ebbcccad52fa313a7bf217f9936b50f3a967
Hugo Chavar <hechavar@gmail.com>
2024-08-02 22:53:05 -0300

    Fix unit test

--- unittests_cash_closing.py
+++ unittests_cash_closing.py
@@ -1,10 +1,16 @@
 import json
 import unittest
 from decimal import Decimal
-from constants import CASH_CLOSING_FILENAME, CASH_CLOSING_UNFORMATTED_FILENAME, TRANSACTIONS_FILENAME
+from constants import CASH_CLOSING_FILENAME, CASH_CLOSING_UNFORMATTED_FILENAME, TRANSACTIONS_FILENAME, BASE_TIMESTAMP, LAST_CASH_POINT_CLOSING_EXPORT_ID, LAST_RECEIPT_NUMBER
+from cash_closing_config import Config
+
+config = Config(BASE_TIMESTAMP, last_cc_export_id=LAST_CASH_POINT_CLOSING_EXPORT_ID, last_receipt_number=LAST_RECEIPT_NUMBER)
+config.cash_register = "e2bc3f5a-1130-4d08-ac54-0fb6730d3963"
+
+config.cash_closing_filename()
 
 # Load the JSON data
-with open(CASH_CLOSING_FILENAME, 'r') as f:
+with open(config.cash_closing_filename(), 'r') as f:
     data = json.load(f)
 
 class TestCashRegisterData(unittest.TestCase):
@@ -15,18 +21,19 @@ class TestCashRegisterData(unittest.TestCase):
 
         # Calculate the sums for each vat_definition_export_id
         for transaction in transactions:
-            for amount in transaction['data']['amounts_per_vat_id']:
-                vat_id = amount['vat_definition_export_id']
-                if vat_id not in vat_sums:
-                    vat_sums[vat_id] = {
-                        'excl_vat': Decimal('0.0'),
-                        'incl_vat': Decimal('0.0'),
-                        'vat': Decimal('0.0')
-                    }
-                vat_sums[vat_id]['excl_vat'] += Decimal(str(amount['excl_vat']))
-                vat_sums[vat_id]['incl_vat'] += Decimal(str(amount['incl_vat']))
-                vat_sums[vat_id]['vat'] += Decimal(str(amount['vat']))
-                # print(f"{transaction['head']['number']}, {vat_id}, {str(amount['excl_vat'])}, {str(amount['incl_vat'])}, {str(amount['vat'])}")
+            if transaction['head']['type'] == 'Beleg':
+                for amount in transaction['data']['amounts_per_vat_id']:
+                    vat_id = amount['vat_definition_export_id']
+                    if vat_id not in vat_sums:
+                        vat_sums[vat_id] = {
+                            'excl_vat': Decimal('0.0'),
+                            'incl_vat': Decimal('0.0'),
+                            'vat': Decimal('0.0')
+                        }
+                    vat_sums[vat_id]['excl_vat'] += Decimal(str(amount['excl_vat']))
+                    vat_sums[vat_id]['incl_vat'] += Decimal(str(amount['incl_vat']))
+                    vat_sums[vat_id]['vat'] += Decimal(str(amount['vat']))
+                    # print(f"{transaction['head']['number']}, {vat_id}, {str(amount['excl_vat'])}, {str(amount['incl_vat'])}, {str(amount['vat'])}")
 
         # Check the sums against the amounts_per_vat_id in the business_cases
         for business_case in data['cash_statement']['business_cases']:
@@ -45,11 +52,12 @@ class TestCashRegisterData(unittest.TestCase):
 
         # Calculate the sums for each payment_type
         for transaction in transactions:
-            for payment in transaction['data']['payment_types']:
-                payment_type = payment['type']
-                if payment_type not in payment_type_sums:
-                    payment_type_sums[payment_type] = Decimal('0.0')
-                payment_type_sums[payment_type] += Decimal(str(payment['amount']))
+            if transaction['head']['type'] == 'Beleg':
+                for payment in transaction['data']['payment_types']:
+                    payment_type = payment['type']
+                    if payment_type not in payment_type_sums:
+                        payment_type_sums[payment_type] = Decimal('0.0')
+                    payment_type_sums[payment_type] += Decimal(str(payment['amount']))
 
         # Check the sums against the payment_type
         for payment_type in data['cash_statement']['payment']['payment_types']:
