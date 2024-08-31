b7a03ce0cee3cab2f3076673848401f573e970bd
Hugo Chavar <hechavar@gmail.com>
2024-08-30 23:43:20 -0300

    Connect transaction fetcher with fiskaly service

--- transaction_fetcher.py
+++ transaction_fetcher.py
@@ -1,10 +1,11 @@
 from fiskaly_service import FiskalyService
 
 class TransactionFetcher:
-    def __init__(self, fiskaly: FiskalyService, last_tx_ok, last_tx):
+    def __init__(self, fiskaly: FiskalyService, client, last_tx_ok, last_tx):
         self.fiskaly_service = fiskaly
         self.last_tx_processed = last_tx_ok
         self.last_tx_pending = last_tx
+        self.client = client
     
     def __iter__(self):
 
@@ -17,26 +18,11 @@ class TransactionFetcher:
             limit = 100 if remaining >= 100 else remaining
             offset = remaining - limit
             print(f"Call fiskaly. \nlast_tx_pending {self.last_tx_pending}\nlast_tx_processed {self.last_tx_processed}\nremaining {remaining}\nlimit {limit}\noffset {offset}")
-            
+            # self.fiskaly_service.get_transactions(self.tss_id, limit, offset)
             self.last_tx_processed += limit
 
-            return self.last_tx_processed
+            # return self.last_tx_processed
+            return self.fiskaly_service.get_transactions(self.client, limit, offset)
         else:
             raise StopIteration
-        
-tf = TransactionFetcher(None, 13797, 14889)
 
-myiter  = iter(tf)
-
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter)) 
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter)) 
-print(next(myiter)) 
-print(next(myiter)) 
