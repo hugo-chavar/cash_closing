dbd00618504296cf137c72e75619f9ed19637df2
Hugo Chavar <hechavar@gmail.com>
2024-09-03 22:38:34 -0300

    Add update_last_tx_pending method to TransactionFetcher

--- transaction_fetcher.py
+++ transaction_fetcher.py
@@ -1,10 +1,9 @@
 from fiskaly_service import FiskalyService
 
 class TransactionFetcher:
-    def __init__(self, fiskaly: FiskalyService, client, last_tx_ok, last_tx):
+    def __init__(self, fiskaly: FiskalyService, client, last_tx_ok):
         self.fiskaly_service = fiskaly
         self.last_tx_processed = last_tx_ok
-        self.last_tx_pending = last_tx # Esto se actualiza internamente no debe venir como param en el constructor
         self.client = client
     
     def __iter__(self):
@@ -18,11 +17,14 @@ class TransactionFetcher:
             limit = 100 if remaining >= 100 else remaining
             offset = remaining - limit
             print(f"Call fiskaly. \nlast_tx_pending {self.last_tx_pending}\nlast_tx_processed {self.last_tx_processed}\nremaining {remaining}\nlimit {limit}\noffset {offset}")
-            # self.fiskaly_service.get_transactions(self.tss_id, limit, offset)
             self.last_tx_processed += limit
 
-            # return self.last_tx_processed
-            return self.fiskaly_service.get_transactions(self.client, limit, offset)
+            return self.get_transactions(limit, offset)
         else:
             raise StopIteration
 
+    def get_transactions(self, limit, offset):
+        return self.fiskaly_service.get_transactions(self.client, limit, offset)
+    
+    def update_last_tx_pending(self):
+        self.last_tx_pending = self.get_transactions(limit=1, offset=0)["count"]
