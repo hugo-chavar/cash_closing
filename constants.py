631117f4507e37a21a3375f8f9ddb7e682033c95
Hugo Chavar <hechavar@gmail.com>
2024-09-11 14:13:28 -0300

    Elimino constante LAST_CASH_POINT_CLOSING_EXPORT_ID

--- constants.py
+++ constants.py
@@ -1,12 +1,14 @@
 import datetime
 from date_tests import get_format_shortdate
+from models import FiskalyClient
+
+client = FiskalyClient.objects.get(id=1)
 
 BASE_DATE_TIME = datetime.datetime(2024, 6, 20, 0, 0)
 BASE_TIMESTAMP = 1718938800 # 1718852400 #1718920800
 SECONDS_PER_DAY = 86400
 
 LAST_CASH_CLOSING_TO_PROCESS = 82 # procesar de a uno, desaparece esta
-LAST_CASH_POINT_CLOSING_EXPORT_ID = 80
 LAST_RECEIPT_NUMBER = 7934
 LAST_PROCESSED_TX_NUMBER = 15897 # es la ultima tx del ultimo del merge_file_filter procesado
 
@@ -14,8 +16,8 @@ LAST_PROCESSED_TX_NUMBER = 15897 # es la ultima tx del ultimo del merge_file_fil
 
 # TIMESTAMP_HIGH = TIMESTAMP_LOW + SECONDS_PER_DAY
 
-date_fmt = get_format_shortdate(BASE_DATE_TIME + datetime.timedelta(days=LAST_CASH_POINT_CLOSING_EXPORT_ID + 1))
-cc_number = LAST_CASH_POINT_CLOSING_EXPORT_ID + 1
+date_fmt = get_format_shortdate(BASE_DATE_TIME + datetime.timedelta(days=client.last_cash_point_closing_export_id + 1))
+cc_number = client.last_cash_point_closing_export_id + 1
 TRANSACTIONS_FILENAME = f'merged7\\merged_file_filter_{date_fmt}.json'
 CASH_CLOSING_FILENAME = f'closings\\CASH_CLOSING_{cc_number:03}_{date_fmt}.json'
 CASH_CLOSING_UNFORMATTED_FILENAME = 'resultUNF_prod_0.json'
