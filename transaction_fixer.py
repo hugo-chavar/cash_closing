6b0042ec577a9120fafd36712d85741bdb9e48d2
Hugo Chavar <hechavar@gmail.com>
2024-09-26 19:38:48 -0300

    Agrego transaction fixer

---
+++ transaction_fixer.py
@@ -0,0 +1,77 @@
+class TransactionFixer:
+    """
+    The purpose of this class is complete some information in the line items. Sometimes the frontend leaves null instead of the item_id. This code fixes that.
+    So, this class completes missing product data in transactions by enriching line items with product information.
+
+    This class is responsible for:
+    - Identifying gaps in transaction numbers
+    - Retrieving product data from a ProductProvider
+    - Updating line items with retrieved product information
+
+    Attributes:
+        product_provider (ProductProvider): Instance of ProductProvider for retrieving product data
+        last_processed_tx_number (int): Last processed transaction number
+
+    Methods:
+        complete_transaction_data(transactions, config): Completes missing product data in transactions
+    """
+    
+    def __init__(self, product_provider):
+        """
+        Initializes the TransactionFixer.
+
+        :param product_provider: An instance of ProductProvider
+        """
+        self.product_provider = product_provider
+        self.last_processed_tx_number = None
+
+    def complete_transaction_data(self, transactions, config):
+        """
+        Completes missing product data in transactions.
+
+        :param transactions: List of transactions
+        :param config: Configuration object with last_processed_tx_number
+        """
+        self.last_processed_tx_number = config.last_processed_tx_number
+
+        for transaction in transactions:
+            self._check_transaction_number_gap(transaction)
+            self._complete_product_data(transaction)
+
+    def _check_transaction_number_gap(self, transaction):
+        """
+        Checks for gaps in transaction numbers.
+
+        :param transaction: Current transaction
+        """
+        if self.last_processed_tx_number != (transaction['number'] - 1):
+            print(f"{self.last_processed_tx_number} => {(transaction['number'] - 1)} falta el siguiente ************ ERROR split\n\n")
+            print(f"Set LAST_PROCESSED_TX_NUMBER to {(transaction['number'] - 1)} \n\n")
+        self.last_processed_tx_number = transaction['number']
+
+    def _complete_product_data(self, transaction):
+        """
+        Completes missing product data in a transaction.
+
+        :param transaction: Current transaction
+        """
+        if "schema" in transaction and "standard_v1" in transaction["schema"]:
+            standard_v1 = transaction["schema"]["standard_v1"]
+            if "order" in standard_v1 and "line_items" in standard_v1["order"]:
+                for line_item in standard_v1["order"]["line_items"]:
+                    self._complete_line_item_data(line_item)
+
+    def _complete_line_item_data(self, line_item):
+        """
+        Completes missing product data in a line item.
+
+        :param line_item: Current line item
+        """
+        if line_item["text"].startswith("null"):
+            try:
+                product_title = line_item["text"].split(' - ')[1]
+                line_item["text"] = str(self.product_provider.get_by_title(product_title))
+            except (AttributeError, IndexError, TypeError) as e:
+                print(f"Error completing line item data: {e}")
+            except KeyError:
+                print(f"Product not found: {product_title}")
